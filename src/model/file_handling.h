339e70a03995879a368b8ddfd48d9db837d29deb
matrinh3 <matrinh33@gmail.com>
2023-05-06 15:07:43 +0700

    nnn

--- src/model/file_handling.h
+++ src/model/file_handling.h
@@ -7,36 +7,21 @@
 #include <string>
 #include <cstdio>
 
+namespace File_Handling{
 using namespace std;
 
-void read(string link_file, ListMayBay& l_mb) {
+template <class List, class Child>
+void read_from_file(string link_file, List& list){
     std::fstream inout;
     inout.open(link_file, ios::in | ios::out);
     if (inout.is_open()) {  // kiểm tra xem file có được mở thành công không
         std::string line;
-        MayBay* temp = nullptr;
+        Child* obj_temp = nullptr;
         while (std::getline(inout, line)) {
-            temp = new MayBay();  // tạo 1 Máy Bay rỗng thông tin
+            obj_temp = new Child();  // tạo 1 Máy Bay rỗng thông tin
             std::istringstream iss(line);
-            iss >> *temp;
-            l_mb.add_from_file_data(temp);
-        }
-        inout.close();
-    } else {
-        throw "Khong the mo file";
-    }
-}
-void read(string link_file, ListChuyenBay& l_mb) {
-    std::fstream inout;
-    inout.open(link_file, ios::in | ios::out);
-    if (inout.is_open()) {  // kiểm tra xem file có được mở thành công không
-        std::string line;
-        ChuyenBay* temp = nullptr;
-        while (std::getline(inout, line)) {
-            temp = new ChuyenBay();
-            std::istringstream iss(line);
-            iss >> *temp;
-            l_mb.add_from_file_data(temp);
+            iss >> *obj_temp;
+            list.add_from_file_data(obj_temp);
         }
         inout.close();
     } else {
@@ -44,25 +29,8 @@ void read(string link_file, ListChuyenBay& l_mb) {
     }
 }
 
-void read(string link_file, TreeHanhKhach& l_mb) {
-    std::fstream inout;
-    inout.open(link_file, ios::in | ios::out);
-    if (inout.is_open()) {  // kiểm tra xem file có được mở thành công không
-        std::string line;
-        HanhKhach* temp = nullptr;
-        while (std::getline(inout, line)) {
-            temp = new HanhKhach();
-            std::istringstream iss(line);
-            iss >> *temp;
-            l_mb.insert(temp);
-        }
-        inout.close();
-    } else {
-        throw "Khong the mo file";
-    }
-}
-
-void write(string link_file, TreeHanhKhach& list_hanhkhach) {
+template<class List>
+void write(string link_file,string link_file_backup, List& list) {
     std::ifstream inputFile(link_file);
     if (!inputFile.is_open()) {
         std::cerr << "Khong mo duoc file " << link_file << std::endl;
@@ -70,8 +38,7 @@ void write(string link_file, TreeHanhKhach& list_hanhkhach) {
     }
     std::string fileContent((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
     inputFile.close();
-
-    std::ofstream backupFile("src/backup/HanhKhach_data_backup.txt", ios::trunc);
+    std::ofstream backupFile(link_file_backup, ios::trunc);
     if (!backupFile.is_open()) {
         std::cerr << "Khong tao duoc file backup" << std::endl;
         return;
@@ -84,79 +51,18 @@ void write(string link_file, TreeHanhKhach& list_hanhkhach) {
         std::cerr << "Khong tao duoc file " << link_file << std::endl;
         return;
     }
-    outputFile << list_hanhkhach;
-    outputFile.close();
-
-    if (remove("src/backup/HanhKhach_data_backup.txt") != 0) {
-        std::cerr << "Khong xoa duoc file backup" << std::endl;
-    }
-}
-
-void write(string file_path_to_load_data, ListChuyenBay& list) {
-    std::ifstream fileToLoadData(file_path_to_load_data);
-    if (!fileToLoadData.is_open()) {
-        std::cerr << "Khong mo duoc file " << file_path_to_load_data << std::endl;
-        return;
-    }
-    std::string fileContent((std::istreambuf_iterator<char>(fileToLoadData)), std::istreambuf_iterator<char>());
-    fileToLoadData.close();
-
-    std::ofstream backupFile("src/backup/ChuyenBay_data_backup.txt", ios::trunc);
-    if (!backupFile.is_open()) {
-        std::cerr << "Khong tao duoc file backup" << std::endl;
-        return;
-    }
-    backupFile << fileContent;
-    backupFile.close();
-
-    std::ofstream outputFile(file_path_to_load_data, ios::trunc);
-    if (!outputFile.is_open()) {
-        std::cerr << "Khong tao duoc file " << file_path_to_load_data << std::endl;
-        return;
-    }
     outputFile << list;
     outputFile.close();
 
-    if (remove("src/backup/ChuyenBay_data_backup.txt") != 0) {
-        std::cerr << "Khong xoa duoc file backup" << std::endl;
+    if (remove(link_file_backup.c_str()) != 0) {
+        std::cerr << "Khong xoa duoc file backup: " << link_file << std::endl;
     }
 }
-
-void write(string file_path_to_load_data, ListMayBay& list) {
-    std::ifstream fileToLoadData(file_path_to_load_data);
-    if (!fileToLoadData.is_open()) {
-        std::cerr << "Khong mo duoc file " << file_path_to_load_data << std::endl;
-        return;
-    }
-    std::string fileContent((std::istreambuf_iterator<char>(fileToLoadData)), std::istreambuf_iterator<char>());
-    fileToLoadData.close();
-
-    std::ofstream backupFile("src/backup/MayBay_data_backup.txt", ios::trunc);
-    if (!backupFile.is_open()) {
-        std::cerr << "Khong tao duoc file backup" << std::endl;
-        return;
-    }
-    backupFile << fileContent;
-    backupFile.close();
-
-    std::ofstream outputFile(file_path_to_load_data, ios::trunc);
-    if (!outputFile.is_open()) {
-        std::cerr << "Khong tao duoc file " << file_path_to_load_data << std::endl;
-        return;
-    }
-    outputFile << list;
-    outputFile.close();
-
-    if (remove("src/backup/MayBay_data_backup.txt") != 0) {
-        std::cerr << "Khong xoa duoc file backup" << std::endl;
-    }
 }
 
 /*
 - đọc dữ liệu từng file
 - lưu dữ liệu vào từng file
-
-
 */
 
 #endif
--- src/model/flight_manager.h
+++ src/model/flight_manager.h
@@ -1,114 +1,120 @@
 #ifndef FLIGHT_MANAGER_H
 #define FLIGHT_MANAGER_H
 
-#include "chuyenbay.h"
-#include "file_handling.h"
-#include "hanhkhach.h"
-#include "maybay.h"
+#include"maybay.h"
+#include"hanhkhach.h"
+#include"chuyenbay.h"
+#include"file_handling.h"
 
-class Flight_Manager {
-   private:
-    ListMayBay ds_maybay;
-    ListChuyenBay ds_chuyenbay;
-    TreeHanhKhach ds_hanhkhach;
+class Flight_Manager{
+    private:
+        ListMayBay ds_maybay;
+        ListChuyenBay ds_chuyenbay;
+        TreeHanhKhach ds_hanhkhach;
+    public:
+        ListMayBay& getListMB(){
+            return ds_maybay;
+        }
+        ListChuyenBay& getListCB(){
+            return ds_chuyenbay;
+        }
+        TreeHanhKhach& getListHK(){
+            return ds_hanhkhach;
+        }
+        Flight_Manager(){
+            File_Handling::read_from_file<TreeHanhKhach,HanhKhach>("src\\data\\data_hanh_khach.txt",ds_hanhkhach);
+            File_Handling::read_from_file<ListMayBay,MayBay>("src\\data\\data_may_bay.txt",ds_maybay);
+            File_Handling::read_from_file<ListChuyenBay,ChuyenBay>("src\\data\\data_chuyen_bay.txt",ds_chuyenbay);
+        }
 
-   public:
-    ListMayBay& getListMB() {
-        return ds_maybay;
-    }
-    ListChuyenBay& getListCB() {
-        return ds_chuyenbay;
-    }
-    TreeHanhKhach& getListHK() {
-        return ds_hanhkhach;
-    }
-    Flight_Manager() {
-        read("src\\data\\data_hanh_khach.txt", ds_hanhkhach);
-        read("src\\data\\data_may_bay.txt", ds_maybay);
-        read("src\\data\\data_chuyen_bay.txt", ds_chuyenbay);
-    }
-
-    ~Flight_Manager() {
-        write(FILE_PATH_DATA_HANH_KHACH, ds_hanhkhach);
-        write(FILE_PATH_DATA_MAY_BAY, ds_maybay);
-        write(FILE_PATH_DATA_CHUYEN_BAY, ds_chuyenbay);
-    }
-
-
-
+        ~Flight_Manager(){
+            File_Handling::write<TreeHanhKhach>(FILE_PATH_DATA_HANH_KHACH,FILE_PATH_DATA_HANH_KHACH_BACKUP,ds_hanhkhach);
+            File_Handling::write<ListMayBay>(FILE_PATH_DATA_MAY_BAY,FILE_PATH_DATA_MAY_BAY_BACKUP,ds_maybay);
+            File_Handling::write<ListChuyenBay>(FILE_PATH_DATA_CHUYEN_BAY,FILE_PATH_DATA_CHUYEN_BAY_BACKUP,ds_chuyenbay);
+        }
 
     Status add_cb(char* ma_so_cb, int minute, int hour, int day, int month, int year, char* san_bay_den, char* so_hieu_mb) {
-        if (ds_chuyenbay.find_by_ma_cb(ma_so_cb)) {
-            return Status("Mã Chuyến Bay Đã Tồn Tại");
-        }
-        if (!ds_maybay.find_mamb(so_hieu_mb)) {
-            return Status("Số Hiệu Máy Bay Không Tồn tại");
-        }
-        Time time(minute, hour, day, month, year);
-        if (!time.isValidDate()) {
-            return Status("Thời Gian Không Hợp Lệ");
-        }
 
-        Time current_time;
-        current_time.get_current_time();
-
-        if (Time::timeDiffInSeconds(current_time, time) < 3600) {
-            return Status("Chỉ Được Lập Chuyến Bay Cách Thời Gian Hiện Tại 1 Giờ");
-        }
-        if (Time::timeDiffInSeconds(current_time, time) > 3600 * 24 * 365) {
-            return Status("Chỉ Được Lập Chuyến Bay Trong Vòng 365 Ngày");
-        }
-        // có case : tồn tại nhiều chuyến bay đúng quy định với mã máy bay này, // sữa sau
-        // thêm cái check kể từ lúc hoàn thành 12h nữa, còn thiếu
-        // ChuyenBay* cb = ds_chuyenbay.find_by_sh_mb_ct(so_hieu_mb);
-        // if(cb != nullptr){
-        //     if(cb->get_trang_thai_cb() == 3){
-        //         return Status("");
-        //     }
-        // }
-        ChuyenBay* new_chuyenbay = new ChuyenBay(ma_so_cb, time, san_bay_den, so_hieu_mb, 1);
-        ds_chuyenbay.insert_order(new_chuyenbay);
-        return Status("Thêm Chuyến Bay Thành Công", Status_Name::SUCCESS);
-    }
-
-    void filter_list_cb(int page, const char* ma_cb, int day, int month, int year, const char* noi_den) {  // 1 page hiển thị 10 line
-        ChuyenBay* current = ds_chuyenbay.get_head();
-        while (current != nullptr) {
-            if (Func_Global::check_prefix(current->get_ma_so_cb(), ma_cb) && Func_Global::check_prefix(current->get_san_bay_den(), noi_den)) {
-                cout << *current << "\n";
+            if(ds_chuyenbay.find_by_ma_cb(ma_so_cb)){
+                return Status("Mã Chuyến Bay Đã Tồn Tại");
             }
-            current = current->get_next();
-        }
-    }
+            if(!ds_maybay.find_mamb(so_hieu_mb)){
+                return Status("Số Hiệu Máy Bay Không Tồn tại");
+            }
+            Time time(minute,hour,day,month,year);
+            if(!time.isValidDate()){
+                return Status("Thời Gian Không Hợp Lệ");
+            }
 
-    void rank_slthcb(int mb[], int& n) {
-        // sau này khắc phục thêm, bây giờ cứ từ từ
-        n = this->ds_maybay.get_so_luong();
-        int vitrimin;
-        int min;
-        for (int i = 0; i < n - 1; i++) {
-            vitrimin = i;
-            for (int j = i + 1; j < n; j++)
-                if (mb[j] < mb[vitrimin]) vitrimin = j;
-            min = mb[vitrimin];
-            mb[vitrimin] = mb[i];
-            mb[i] = min;
-        }
-    }
+            Time current_time;
+            current_time.get_current_time();
 
-    void in_ds_kh(const char* ma_so_cb) {
-        ChuyenBay* p = ds_chuyenbay.find_by_ma_cb_ct(ma_so_cb);
-        if (p != nullptr) {
-            MayBay* mb = ds_maybay.find_mamb_ct(p->get_so_hieu_mb());
-            for (int i = 0; i < mb->getSoDay(); i++) {
-                for (int j = 0; j < mb->getSoDong(); j++) {
-                    if (p->get_listve()[i][j] != nullptr) {
-                        HanhKhach* hk = ds_hanhkhach.search(p->get_listve()[i][j]->getSoCMND());
-                    }
+            if(Time::timeDiffInSeconds(current_time,time) < 3600 ){
+                return Status("Chỉ Được Lập Chuyến Bay Cách Thời Gian Hiện Tại 1 Giờ");
+            }
+            if(Time::timeDiffInSeconds(current_time,time) > 3600 * 24 * 365 ){ 
+                return Status("Chỉ Được Lập Chuyến Bay Trong Vòng 365 Ngày");
+            }
+            // có case : tồn tại nhiều chuyến bay đúng quy định với mã máy bay này, // sữa sau
+            // thêm cái check kể từ lúc hoàn thành 12h nữa, còn thiếu
+            // ChuyenBay* cb = ds_chuyenbay.find_by_sh_mb_ct(so_hieu_mb);
+            // if(cb != nullptr){
+            //     if(cb->get_trang_thai_cb() == 3){
+            //         return Status("");
+            //     }
+            // }
+            ChuyenBay* new_chuyenbay = new ChuyenBay(ma_so_cb, time, san_bay_den, so_hieu_mb, 1);
+            ds_chuyenbay.insert_order(new_chuyenbay);
+            return Status("Thêm Chuyến Bay Thành Công",Status_Name::SUCCESS);
+        }
+
+
+        void filter_list_cb(int page, const char * ma_cb, int day, int month, int year, const char* noi_den){ //1 page hiển thị 10 line
+            ChuyenBay* current = ds_chuyenbay.get_head();
+            while (current != nullptr) {
+                if(Func_Global::check_prefix(current->get_ma_so_cb(),ma_cb) && Func_Global::check_prefix(current->get_san_bay_den(),noi_den)){
+                    cout << *current << "\n";
                 }
+                current = current->get_next();
+            }
+        }
+
+        void rank_slthcb(int mb[], int &n){
+            // sau này khắc phục thêm, bây giờ cứ từ từ
+            n = this->ds_maybay.get_so_luong();
+            int vitrimin;
+            int min;
+            for (int i = 0; i < n-1; i++){
+                vitrimin = i;
+                for (int j = i+1; j < n; j++)
+                    if (mb[j] < mb[vitrimin]) vitrimin=j;
+                min = mb[vitrimin];
+                mb[vitrimin] = mb[i];  
+                mb[i] = min;
+            }
+        }
+
+
+        void in_ds_kh( const char* ma_so_cb){
+   ChuyenBay* p= ds_chuyenbay.find_by_ma_cb_ct(ma_so_cb);
+  cout<<"1";
+   if(p!=nullptr){
+    MayBay* mb=ds_maybay.find_mamb_ct(p->get_so_hieu_mb());
+    for(int i=0; i<mb->getSoDay();i++){
+         cout<<"2";
+        for(int j=0;j<mb->getSoDong();j++){
+             cout<<"3";
+            if(p->get_listve()[i][j] != nullptr){
+            HanhKhach* hk= ds_hanhkhach.search(p->get_listve()[i][j]->getSoCMND());
+             cout<<"4";
+            cout<< hk;
+
             }
         }
     }
+     
+}
+}
 };
 
 #endif
